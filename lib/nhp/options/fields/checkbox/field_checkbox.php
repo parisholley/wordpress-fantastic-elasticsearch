d84f1823428ba641245142bd9bc63fc2919cbcbe
Eric Bloch <eric.bloch@gmail.com>
2013-07-06 13:08:44 -0400

    Fix null check

--- lib/nhp/options/fields/checkbox/field_checkbox.php
+++ lib/nhp/options/fields/checkbox/field_checkbox.php
@@ -30,7 +30,7 @@ class NHP_Options_checkbox extends NHP_Options{
 		
 		$class = (isset($this->field['class']))?$this->field['class']:'';
 		
-		echo ($this->field['desc'] != '')?' <label for="'.$this->field['id'].'">':'';
+		echo (isset($this->field['desc']) && ($this->field['desc'] != ''))?' <label for="'.$this->field['id'].'">':'';
 		
 		echo '<input type="checkbox" id="'.$this->field['id'].'" name="'.$this->args['opt_name'].'['.$this->field['id'].']" value="1" class="'.$class.'" '.checked($this->value, '1', false).'/>';
 		
