cd0494fa7d770c501e7ff523574240a1bb3baa22
Paris Holley <mail@parisholley.com>
2013-07-11 00:31:41 -0500

    hoping this would fix random no shard error

--- integration-tests/elasticsearch/BaseIntegrationTestCase.php
+++ integration-tests/elasticsearch/BaseIntegrationTestCase.php
@@ -12,8 +12,10 @@ class BaseIntegrationTestCase extends BaseTestCase
 
 		$this->index = Api::index(false);
 		$this->index->create(array(), true);
-		$this->index->open();
-		
+
+		// see http://elasticsearch-users.115913.n3.nabble.com/How-to-wait-for-a-CreateIndexRequest-to-really-finish-using-java-TransportClient-td4027828.html
+		$this->index->addAlias('foobar');
+
 		$this->assertEquals(0, $this->index->count());
 	}
 }
