24159646938a8eb45386bf2b214973fbda9e8533
Paris Holley <mail@parisholley.com>
2013-07-21 09:51:51 -0700

    ignore downloads, adding vendor

---
+++ vendor/jeremykendall/php-domain-parser/tests/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+error_reporting(-1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+date_default_timezone_set('America/Chicago');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+function d($expression)
+{
+    var_dump($expression);
+}
+
+function dd($expression)
+{
+    d($expression);
+    die();
+}
