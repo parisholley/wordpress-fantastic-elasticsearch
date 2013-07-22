ad97d469126669364d3aeaa5428383b058aced07
Paris Holley <mail@parisholley.com>
2013-07-21 19:52:07 -0500

    gonna use submodule

--- docs/api/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","elasticsearch\\Config"],["c","elasticsearch\\Defaults"],["c","elasticsearch\\Faceting"],["c","elasticsearch\\Indexer"],["c","elasticsearch\\Searcher"]];
