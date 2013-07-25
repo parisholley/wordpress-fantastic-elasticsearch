36649de4f01a34aa970e8aa0869e11274db0d082
Paris Holley <mail@parisholley.com>
2013-07-24 23:06:53 -0500

    smarter waiting instead of sleep

--- tests/selenium.sh
+++ tests/selenium.sh
@@ -1,8 +1,12 @@
 cd `dirname $0`
 
+echo "Detecting if phantomjs is installed."
+
 type phantomjs --help >/dev/null 2>&1 
 
 if [ ! $? -eq 0 ]; then
+	echo "Installing phantomjs."
+
 	brew update && brew install phantomjs
 fi
 
@@ -15,6 +19,7 @@ cd work
 VERSION="selenium-server-standalone-2.33.0.jar"
 
 if [ ! -f $VERSION ]; then
+	echo "Downloading selenium."
 	wget https://selenium.googlecode.com/files/$VERSION
 fi
 
@@ -24,17 +29,42 @@ if [ -f "selenium.pid" ]; then
 	rm selenium.pid
 fi
 
+if [ -f "selenium.log" ]; then
+	rm selenium.log
+fi
+
 ps -ef | grep "java -jar selenium-server" | grep -v grep &>/dev/null
 
 if [ $? -eq 0 ]; then
 	echo "WARNING: A version of selenium is already running."
 fi
 
-java -jar $VERSION -role hub &>/dev/null &
+echo -n "Starting selenium service."
+
+java -jar $VERSION -role hub &> selenium.log &
 
 echo "$!" > selenium.pid
 
-sleep 5s
+REGISTERED=0
+
+while [ $REGISTERED -eq 0 ]; do
+	grep 'Started SocketConnector@0.0.0.0:4444' selenium.log &>/dev/null
+
+	if [ $? -eq 0 ]; then
+		echo -ne "(Complete)\n"
+		REGISTERED=1
+	else
+		echo -n "."
+		sleep 1s
+
+		ps -ef | grep "selenium-server" | grep -v grep  &>/dev/null
+
+		if [ $? -eq 1 ]; then
+			echo "ERROR: selenium died"
+			exit 1
+		fi
+	fi
+done
 
 if [ -f "phantom.pid" ]; then
 	kill -9 `cat phantom.pid` &>/dev/null
@@ -52,19 +82,42 @@ phantomjs --webdriver=127.0.0.1:8910 --webdriver-selenium-grid-hub=http://127.0.
 
 echo "$!" > phantom.pid
 
-echo "Waiting for phantomjs to register with grid.."
+echo -n "Waiting for phantomjs to register with grid."
 
-sleep 10s
+REGISTERED=0
+
+while [ $REGISTERED -eq 0 ]; do
+	curl -XGET http://localhost:4444/grid/api/proxy/?id=http://127.0.0.1:8910 2>/dev/null | grep 'registerCycle' &>/dev/null
+
+	if [ $? -eq 0 ]; then
+		echo -ne "(Complete)\n"
+		REGISTERED=1
+	else
+		echo -n "."
+		sleep 1s
+
+		ps -ef | grep "phantomjs" | grep -v grep  &>/dev/null
+
+		if [ $? -eq 1 ]; then
+			echo "ERROR: phantomjs died"
+			exit 1
+		fi
+	fi
+done
+
+echo "Detecting phpunit/PHPUnit_Selenium pear extension is installed."
 
 type pear >/dev/null 2>&1 || { echo >&2 "Pear is not installed."; exit 1; }
 
 echo '<?php require "PHPUnit/Autoload.php"; require "PHPUnit/Extensions/Selenium2TestCase.php"; ?>' | php &>/dev/null
 
 if [ ! $? -eq 0 ]; then
+	echo "Installing phpunit/PHPUnit_Selenium."
 	pear install phpunit/PHPUnit_Selenium
 fi
 
 if [ ! -d "vagrantpress-wordpress-fantastic-elasticsearch" ]; then
+	echo "Downloading vagrant information."
 	wget https://github.com/parisholley/vagrantpress/archive/wordpress-fantastic-elasticsearch.zip
 	unzip wordpress-fantastic-elasticsearch.zip
 	rm wordpress-fantastic-elasticsearch.zip
@@ -79,6 +132,8 @@ if [ -d $PLUGIN_DIR ]; then
 	rm -rf $PLUGIN_DIR
 fi
 
+echo "Copying plugin into VM's wordpress plugin folder."
+
 mkdir $PLUGIN_DIR
 cp -rf ../../../elasticsearch.php $PLUGIN_DIR/
 cp -rf ../../../src/ $PLUGIN_DIR/src
@@ -86,6 +141,8 @@ cp -rf ../../../vendor/ $PLUGIN_DIR/vendor
 cp -rf ../../../wp/ $PLUGIN_DIR/wp
 
 cd ..
+
+echo "Running tests."
 
 phpunit --no-configuration --verbose --bootstrap="../selenium-tests/bootstrap.php" ../selenium-tests;
 
@@ -101,7 +158,13 @@ if [ -f "selenium.pid" ]; then
 	rm selenium.pid;
 fi
 
+if [ -f "selenium.log" ]; then
+	rm selenium.log
+fi
+
 if [ $? -eq 0 ]; then
+	echo "Suspending VM."
+
 	cd vagrantpress-wordpress-fantastic-elasticsearch;
 
 	vagrant suspend
