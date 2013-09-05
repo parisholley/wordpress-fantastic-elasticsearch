3a5a7644c2c035f4eb20b472283ab69db808c644
Paris Holley <mail@parisholley.com>
2013-09-04 22:20:08 -0600

    used appropriate is tag

--- wp/theme/archive.php
+++ wp/theme/archive.php
@@ -5,7 +5,7 @@ class Archive extends AbstractArchive{
 	function facets($wp_query, $args){
 		$enabled = Config::option('enable_all_posts', false);
 
-		if(!$enabled || !is_archive() || !isset($wp_query->query_vars['post_type']) ){
+		if(!$enabled || !is_post_type_archive() || !isset($wp_query->query_vars['post_type']) ){
 			return;
 		}
 
