18dfac8c60ba2879e439b3b18c9fdca8f9664fc1
Paris Holley <mail@parisholley.com>
2016-04-26 22:26:48 -0700

    fix for #71

--- wp/admin/hooks.php
+++ wp/admin/hooks.php
@@ -1,41 +1,51 @@
 <?php
 namespace elasticsearch;
 
-class Hooks{
-	function __construct(){
-		add_action( 'save_post', array( &$this, 'save_post' ) );
-		add_action( 'delete_post', array( &$this, 'delete_post' ) );
-		add_action( 'trash_post', array( &$this, 'delete_post' ) );
+class Hooks
+{
+	function __construct()
+	{
+		add_action('save_post', array(&$this, 'save_post'));
+		add_action('delete_post', array(&$this, 'delete_post'));
+		add_action('trash_post', array(&$this, 'delete_post'));
+		add_action('transition_post_status', array(&$this, 'transition_post'), 10, 3);
 	}
-	
-	function save_post( $post_id ) {
-		if(is_object( $post_id )){
+
+	function save_post($post_id)
+	{
+		if (is_object($post_id)) {
 			$post = $post_id;
 		} else {
-			$post = get_post( $post_id );
+			$post = get_post($post_id);
 		}
 
-		if($post == null || !in_array($post->post_type, Config::types())){
+		if ($post == null || !in_array($post->post_type, Config::types())) {
 			return;
 		}
 
-		if ($post->post_status == 'trash'){
-			Indexer::delete($post);
-		}
-
-		if ($post->post_status == 'publish'){
+		if ($post->post_status == 'publish') {
 			Indexer::addOrUpdate($post);
+		} else {
+			Indexer::delete($post);
 		}
 	}
 
-	function delete_post( $post_id ) {
-		if(is_object( $post_id )){
+	function transition_post($new_status, $old_status, $post)
+	{
+		if ($new_status != 'publish' && $new_status != $old_status) {
+			Indexer::delete($post);
+		}
+	}
+
+	function delete_post($post_id)
+	{
+		if (is_object($post_id)) {
 			$post = $post_id;
 		} else {
-			$post = get_post( $post_id );
+			$post = get_post($post_id);
 		}
 
-		if($post == null || !in_array($post->post_type, Config::types())){
+		if ($post == null || !in_array($post->post_type, Config::types())) {
 			return;
 		}
 
